7f72c44b4b493324019c8f7956b805cec97f249f
Szymon <szymonpakula98@gmail.com>
2020-01-20 23:05:24 +0100

    login redirection

--- front/src/components/AppBar/index.jsx
+++ front/src/components/AppBar/index.jsx
@@ -4,18 +4,68 @@ import { Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 const AppBar = () => {
-  return (
-      <Menu size ="massive" fluid widths={8}>
-        <Menu.Item   /><br></br>
-        <Menu.Item  icon="home" color='grey' as={NavLink} to="/" activeClassName="active" exact /><br></br>
-        <Menu.Item  icon="paw" color='green' as={NavLink} to="/animals" activeClassName="active" exact /><br></br>
-        <Menu.Item  icon="users" color='blue' as={NavLink} to="/workers" activeClassName="active" exact /><br></br>
-        <Menu.Item  icon="chess board" color='brown' as={NavLink} to="/enclosures" activeClassName="active" exact /><br></br>
-        <Menu.Item  icon="doctor" color='grey' as={NavLink} to="/vets" activeClassName="active" exact /><br></br>
-        <Menu.Item  icon="setting" color='grey' as={NavLink} to="/settings" activeClassName="active" exact /><br></br>
-        <Menu.Item  /><br></br>
-      </Menu>
-  );
+    return (
+        <Menu size="massive" fluid widths={8}>
+            <Menu.Item />
+            <br></br>
+            <Menu.Item
+                icon="home"
+                color="grey"
+                as={NavLink}
+                to="/"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item
+                icon="paw"
+                color="green"
+                as={NavLink}
+                to="/animals"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item
+                icon="users"
+                color="blue"
+                as={NavLink}
+                to="/workers"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item
+                icon="chess board"
+                color="brown"
+                as={NavLink}
+                to="/enclosures"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item
+                icon="doctor"
+                color="grey"
+                as={NavLink}
+                to="/vets"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item
+                icon="setting"
+                color="grey"
+                as={NavLink}
+                to="/settings"
+                activeClassName="active"
+                exact
+            />
+            <br></br>
+            <Menu.Item />
+            <br></br>
+        </Menu>
+    );
 };
 
 export default AppBar;
--- front/src/components/Homepage/HomepageContent.jsx
+++ front/src/components/Homepage/HomepageContent.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Grid, Segment, GridColumn } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
 import WorkerInfo from './WorkerInfo';
 import NavButton from './NavButton';
+import Store from '../../Store';
 
 class HomepageContent extends React.Component {
     state = {
         
     }
     
+    static contextType = Store;
+
     render() {
+        if (!this.context.isLogged) return <Redirect to="/login" />;
         return (
             <Grid columns={3} divided>
                 <Grid.Row>
