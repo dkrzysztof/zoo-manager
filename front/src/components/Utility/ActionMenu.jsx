89f80ae783c509bbc1d0d570a677f2e4a0fbca2e
Szymon <szymonpakula98@gmail.com>
2020-01-21 04:45:20 +0100

    modals

--- front/src/components/Utility/ActionMenu.jsx
+++ front/src/components/Utility/ActionMenu.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import {Icon, Button, Segment} from 'semantic-ui-react';
+import { Icon, Button, Segment, Modal } from 'semantic-ui-react';
 
 class ActionMenu extends React.Component {
+    state = { visible: false };
 
-    
     render() {
         return (
-            <Segment compact >
-                <Button>
-                    <Icon name="plus" size="huge"/>
-                </Button>
+            <Segment compact>
+                <Modal
+                    trigger={
+                        <Button>
+                            <Icon name="plus" size="huge" />
+                        </Button>
+                    }
+                    content="Dodaj"
+                ></Modal>
                 <br></br>
                 <br></br>
-                <Button>
-                    <Icon name="minus" size="huge"/>
-                </Button>
-                <br></br>
-                <br></br>
-                <Button>
-                    <Icon name="edit outline" size="huge" />
-                </Button>
+                <Modal
+                    trigger={
+                        <Button>
+                            <Icon name="minus" size="huge" />
+                        </Button>
+                    }
+                    content="Usuń"
+                ></Modal>
 
+                <br></br>
+                <br></br>
+                <Modal
+                    trigger={
+                        <Button>
+                            <Icon name="edit outline" size="huge" />
+                        </Button>
+                    }
+                    content="Zmień"
+                ></Modal>
             </Segment>
         );
     }
